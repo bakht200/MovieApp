9bf88a2ab1df46b4fe09208a1fd1587373167603
bakht biland <62112170+bakht200@users.noreply.github.com>
2023-10-12 23:20:28 +0500

    implementation of movie app

---
+++ lib/constant/config.dart
@@ -0,0 +1,14 @@
+class Config {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String imagesUrl = "https://image.tmdb.org/t/p/w500/";
+}
+
+class Routes {
+  static const String upcomingMovieList = "/movie/upcoming";
+  static const String movieCategoryList = "/genre/movie/list";
+}
+
+class AuthorizationToken {
+  static const String autheorizationToekn =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmUyNDUzZmUyZmYyYmNmYWFhYmQ0ODFjYzgxNWViZSIsInN1YiI6IjY1MjYzZDIxYzUyNWM0MDBhY2JiNTJlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nhwiCAy1ALXUCflitPtz6cH71TUtyB7dx8Iufoeizvs";
+}
